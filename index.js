d66785abbb83de1a2e950a6988261de3ffa4bddd
Tonidev <antoniodev2709@gmail.com>
2026-01-05 16:18:37 -0300

    Innitial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+app.post("/users", (req, res) => {
+  const body = req.body;
+  users.push(body);
+  res.status(201).send("usuário criado com sucesso!");
+});
+
+app.get("/users", (req, res) => {
+  res.send({ users });
+});
+
+app.listen(3003, () => {
+  console.log("Server is running on port 3003");
+});
